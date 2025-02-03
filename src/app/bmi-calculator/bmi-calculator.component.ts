dae1068664a0b252459a891ab7cbf4af0a9651b0
rob-schlenker <schlenk7@gmail.com>
2025-02-03 11:06:18 -0500

    initial commit

---
+++ src/app/bmi-calculator/bmi-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bmi-calculator',
+  imports: [],
+  templateUrl: './bmi-calculator.component.html',
+  styleUrl: './bmi-calculator.component.css'
+})
+export class BmiCalculatorComponent {
+
+}
